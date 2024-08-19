d31466d3b46c57a17d4b9b6911e5ae0a9f89ff08
Zac <yihongle@buffalo.edu>
2024-08-19 09:00:19 +0800

    pass 1st stage

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"bufio"
-	// Uncomment this block to pass the first stage
-	// "fmt"
+	"fmt"
 	"os"
 )
 
 func main() {
 	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
